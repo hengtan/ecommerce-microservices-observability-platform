6d31b58e894fde0e309b20332aaeb12c9f4d9673
hengtan <hengjt1@gmail.com>
2025-04-18 13:56:25 -0300

    chore: update assembly informational versions and NuGet tool version to 6.13.2

--- src/Services/Orders/Orders.API/obj/Debug/net9.0/Orders.API.AssemblyInfo.cs
+++ src/Services/Orders/Orders.API/obj/Debug/net9.0/Orders.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Orders.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1549b48beaabe23cf4434f2331c3a5ee6de5b8de")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c1783bc66dcb85d8592c28b12888ed8eb5180fe6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Orders.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Orders.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
